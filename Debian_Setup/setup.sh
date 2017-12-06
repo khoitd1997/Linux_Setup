ab1f9e58bb4762c4ca3d6dfb4715b73b238898ef
khoitd1997 <khoidinhtrinh@gmail.com>
2017-12-06 12:55:33 -0800

    quick variable fix

--- Debian_Setup/setup.sh
+++ Debian_Setup/setup.sh
@@ -8,7 +8,7 @@
 #NO SPACE AROUND '=' for variable assignment
 
 #list of general utilities without GUI
-SOFTWARE_GENERAL_REPO_NON_GUI=" checkinstall lm-sensors cmake \ valgrind gcc clang llvm  emacs build-essential htop net-tools "
+SOFTWARE_GENERAL_REPO_NON_GUI=" checkinstall lm-sensors cmake valgrind gcc clang llvm  emacs build-essential htop net-tools "
 
 #list of software with GUI                        
 SOFTWARE_WITH_GUI= " gksu terminator gitg guake ddd evince synaptic psensor gufw "
