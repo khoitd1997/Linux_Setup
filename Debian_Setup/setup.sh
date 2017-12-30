975d8a24cc698efce776e93b0733d7c0bf1f4bab
khoitd1997 <khoidinhtrinh@gmail.com>
2017-12-30 14:23:11 -0800

    added screen and minicom

--- Debian_Setup/setup.sh
+++ Debian_Setup/setup.sh
@@ -8,7 +8,7 @@
 #NO SPACE AROUND '=' for variable assignment
 
 #list of general utilities without GUI
-SOFTWARE_GENERAL_REPO_NON_GUI=" doxygen xclip checkinstall lm-sensors cmake valgrind gcc clang llvm emacs build-essential htop net-tools gnome-keyring libsecret "
+SOFTWARE_GENERAL_REPO_NON_GUI=" doxygen xclip checkinstall lm-sensors cmake valgrind gcc clang llvm emacs build-essential htop net-tools gnome-keyring minicom screen "
 
 #list of software with GUI                        
 SOFTWARE_WITH_GUI=" gksu terminator guake ddd evince synaptic psensor gufw "
