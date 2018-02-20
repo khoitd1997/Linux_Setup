6c1eed9c6882096804be0228740cc48214dbfb6f
khoitd1997 <khoidinhtrinh@gmail.com>
2018-02-19 21:40:16 -0800

    change pip to python3

--- Debian_Setup/setup.sh
+++ Debian_Setup/setup.sh
@@ -8,7 +8,7 @@
 #NO SPACE AROUND '=' for variable assignment
 
 #list of general utilities without GUI
-SOFTWARE_GENERAL_REPO_NON_GUI=" doxygen checkinstall lm-sensors cmake valgrind gcc clang llvm emacs build-essential htop net-tools  minicom screen python-pip"
+SOFTWARE_GENERAL_REPO_NON_GUI=" doxygen checkinstall lm-sensors cmake valgrind gcc clang llvm emacs build-essential htop net-tools  minicom screen python3-pip"
 
 #list of software with GUI
 SOFTWARE_WITH_GUI=" gksu terminator guake ddd evince synaptic psensor gufw xpad unity-tweak-tool libreoffice-style-hicontrast unattended-upgrades gparted libappindicator1 libindicator7 hardinfo chromium-browser"
