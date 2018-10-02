1da6dfc23052eaa5a46e7552f4d35f3e1da98d5b
khoitd1997 <khoidinhtrinh@gmail.com>
2018-10-01 19:23:08 -0700

    fix username and package issue for VM

--- Debian_Setup/VM/setup_VM.sh
+++ Debian_Setup/VM/setup_VM.sh
@@ -9,7 +9,6 @@ software=" calibre ufw "
 set -e
 
 print_message "Starting VM setup scripts\n"
-sudo apt remove libappstream3
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install ${software} -y
@@ -25,15 +24,15 @@ kernel_name=$(uname -r)
 #deleting the OS built-in guest addition to avoid conflict
 sudo rm -rf /lib/modules/${kernel_name}/kernel/ubuntu/vbox/vbox*
 
-
+current_user="$(logname)"
 #run guest addition install
-cd /media/${USER}/VBox_GAs*
+cd /media/${current_user}/VBox_GAs*
 sudo ./VBoxLinux*
 
-sudo usermod -a -G vboxsf ${USER}
+sudo usermod -a -G vboxsf ${current_user}
 
 print_message "Done installing, rebooting in 5\n"
 sleep 5
 
-sudo apt autoremove 
+sudo apt autoremove -y
 reboot
