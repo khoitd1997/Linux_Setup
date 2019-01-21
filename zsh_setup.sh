ca2641d56f4631d373a4c0a599510452dbbc834d
khoitd1997 <khoidinhtrinh@gmail.com>
2019-01-20 22:06:47 -0800

    added zsh installation

---
+++ zsh_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# written for installation of zsh
+source utils.sh
+
+zsh_plugin="https://github.com/zsh-users/zsh-completions.git \
+            https://github.com/zsh-users/zsh-autosuggestions.git"
+
+#--------------------------------------------------------------------------------------
+
+print_message "Starting zsh installation"
+sudo apt update && sudo apt install zsh zsh-syntax-highlighting
+
+# oh-my-zsh stuffs
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+cp zsh/.zshrc ~/.zshrc
+
+# plugins
+cd ~/.oh-my-zsh/custom/plugins
+for plugin in ${zsh_plugin}; do 
+git clone ${plugin}
+done
+
+print_message "zsh installation done"
