3e1e9c7bc9394fd6377754432a225e09d41a0781
khoitd1997 <khoidinhtrinh@gmail.com>
2019-01-21 18:15:47 -0800

    minor change to debug read problem

--- RPM_Setup/setup.sh
+++ RPM_Setup/setup.sh
@@ -136,18 +136,20 @@ done
 esac
 done 
 
-print_message "Installing vscode, click any key to proceed"
+print_message "Installing vscode, click any key to proceed\n"
 empty_input_buffer()
 read input
+
 # got from here: https://code.visualstudio.com/docs/setup/linux
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
 sudo dnf install code -y
 
-print_message "Installing zsh, click any key to proceed"
+print_message "Installing zsh, click any key to proceed\n"
 empty_input_buffer()
 read input
+
 sudo dnf install zsh zsh-syntax-highlighting -y
 source ../zsh_setup.sh
 
