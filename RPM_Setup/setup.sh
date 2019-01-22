5170d25810a66cd3292c359cf6c8f2ec58d04394
khoitd1997 <khoidinhtrinh@gmail.com>
2019-01-21 17:19:46 -0800

    added more -y for faster install

--- RPM_Setup/setup.sh
+++ RPM_Setup/setup.sh
@@ -49,23 +49,20 @@ sudo passwd -l root
 sudo systemctl status firewalld
 
 sudo dnf update && sudo dnf upgrade -y
-sudo dnf install ${software_group}
+sudo dnf install ${software_group} -y
 # got from here: https://rpmfusion.org/Configuration/
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update 
 
 sudo dnf config-manager --set-enabled google-chrome rpmfusion-nonfree-nvidia-driver
-sudo dnf groupinstall multimedia
-sudo dnf groupupdate multimedia
-sudo dnf groupupdate sound-and-video
+sudo dnf groupinstall multimedia -y
+sudo dnf groupupdate multimedia -y
+sudo dnf groupupdate sound-and-video -y
 
 sudo dnf update
-sudo dnf install ${SOFTWARE_GENERAL_REPO}
-
-
+sudo dnf install ${SOFTWARE_GENERAL_REPO} -y
 
 #flatpak
-
 for flatpak in ${flatpak_package} ; do
 	sudo flatpak install flathub ${flatpak} -y 
 done
@@ -148,12 +145,12 @@ read input
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
-sudo dnf install code
+sudo dnf install code -y
 
 print_message "Installing zsh, click any key to proceed"
 empty_input_buffer()
 read input
-sudo dnf install zsh zsh-syntax-highlighting
+sudo dnf install zsh zsh-syntax-highlighting -y
 source ../zsh_setup.sh
 
 timedatectl set-local-rtc 1 --adjust-system-clock # adjust clock to local
