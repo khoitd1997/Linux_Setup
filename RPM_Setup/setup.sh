b3dee09a4a93d7276f696fd58d558081210cd64a
khoitd1997 <khoidinhtrinh@gmail.com>
2019-02-08 15:24:19 -0800

    added flameshot to startup list

--- RPM_Setup/setup.sh
+++ RPM_Setup/setup.sh
@@ -10,7 +10,7 @@ software_group=" @development-tools fedora-workstation-repositories "
 
 software_dropped_not_in_repo=" checkinstall build-essential gufw hardinfo "
 
-software_startup=" redshift synapse "
+software_startup=" redshift synapse flameshot "
 
 # list of general utilities without GUI
 software_general_repo_non_gui=" doxygen cmake valgrind \
